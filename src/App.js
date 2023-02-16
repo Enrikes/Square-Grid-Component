822b4ae717aeb38d43d00ce0fe2a47448a6f831c
Enrique Troche <55448347+Enrikes@users.noreply.github.com>
2023-02-15 23:58:05 -0500

    Initial component commit.

---
+++ src/App.js
@@ -0,0 +1,58 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="container">
+      <div className="child">
+        <div className="caption-container">
+          <div className="content">
+            <h1 id="sm-font">Villager Torture!</h1>
+            <p id="reg-font">Da Villies out here.</p>
+          </div>
+          <div className="img"></div>
+        </div>
+      </div>
+      <div className="child-2">
+        <div className="caption-container">
+          <div className="content">
+            <h1 id="sm-font">The Cow Homie!</h1>
+            <p id="reg-font">Da Villies out here.</p>
+          </div>
+          <div className="img-2"></div>
+        </div>
+      </div>
+      <div className="child-3">
+        <div className="caption-container">
+          <div className="content">
+            <div className="title">
+              <h1 id="sm-font">Spoopy Cave</h1>
+            </div>
+            <p id="reg-font">Da Villies out here.</p>
+          </div>
+          <div className="img-3"></div>
+        </div>
+      </div>
+      <div className="child-4">
+        <div className="caption-container">
+          <div className="content">
+            <h1 id="sm-font">LOD In Minecraft</h1>
+            <p id="reg-font">Wow! What a wonderful view.</p>
+          </div>
+          <div className="img-4"></div>
+        </div>
+      </div>
+      <div className="child-5"></div>
+      <div className="child-6">
+        <div className="caption-container">
+          <div className="content">
+            <h1 id="sm-font">LOD In Minecraft</h1>
+            <p id="reg-font">Wow! What a wonderful view.</p>
+          </div>
+          <div className="img-6"></div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
